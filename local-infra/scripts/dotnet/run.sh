d753366ef3ba941a0d9d60e875c80dbd7aae546a
6daniel32 <6daniel32@gmail.com>
2024-11-15 00:01:22 +0000

    Improve run command

--- local-infra/scripts/dotnet/run.sh
+++ local-infra/scripts/dotnet/run.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-# compile the application
-docker start compiler
-
-# await until the compilation ends
-while [ "$(docker inspect -f '{{.State.Running}}' compiler)" = true ];
-do
-    echo "Compiling...";
-    sleep 1;
-done
-
 # Check if the runtime is up
 if [ "$(docker ps -q -f name=runtime)" ]; then
-    echo "Server restarted";
-    docker restart runtime;
-else
-    # If the runtime is not running, start the whole environment
-    echo "Server started";
-    docker-compose up -d;
+    # Compile the application
+    docker start compiler > /dev/null;
+
+    # Await until the compilation ends
+    while [ "$(docker inspect -f '{{.State.Running}}' compiler)" = true ];
+    do
+        echo "Compiling...";
+        sleep 1;
+    done
+    
+    # Redeploy the app with the new version of the binary
+    echo $'\nRestarting server...';
+    docker restart runtime  > /dev/null;
+    echo $'Server restarted';
+    exit 0;
 fi
+
+# Build the containers if they do not exist.
+if [ ! "$(docker ps -q -a -f name=runtime)" ]; then
+    echo $'Building server...\n';
+    docker compose build;
+    echo $'\nServer successfully built\n';
+fi
+
+# The runtime container was not running, start 
+# the whole environment.
+echo $'Starting server...\n';
+docker compose up -d;
+echo $'\nServer started';
+exit 0;
